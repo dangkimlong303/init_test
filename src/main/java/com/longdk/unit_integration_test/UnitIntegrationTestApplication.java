c618adfda5a7c3db39e36d7266dc83d26098572f
Dang Kim Long <long.dang@fruitful.io>
2024-02-20 20:30:03 +1100

    init

---
+++ src/main/java/com/longdk/unit_integration_test/UnitIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.longdk.unit_integration_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitIntegrationTestApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(UnitIntegrationTestApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/longdk/unit_integration_test/UnitIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longdk.unit_integration_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitIntegrationTestApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
